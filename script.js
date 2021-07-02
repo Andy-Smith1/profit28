9f7ee50c89a9d327dd9deb00515d1713b68d0644
Andy <84683649+Andy-Smith1@users.noreply.github.com>
2021-07-02 17:02:23 +0100

    Added working lightbox gallery

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+
+//hamburger navbar
 const toggleButton = document.querySelector('.toggle-button');
 const navbarRight = document.querySelector('.navbar-right');
-
-
 
 toggleButton.addEventListener('click', () => {
     navbarRight.classList.toggle('active');
 });
 
+//gallery lightbox
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
 
+const galleryImages = document.querySelectorAll('.gallery-img');
+galleryImages.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active'); //adds dark overlay to page
+        const img = document.createElement('img');
+        img.src = image.src;
+        img.classList.add('lightbox-image');//adds class for styling
+        while (lightbox.firstChild) {
+            //removes any existing image before appending
+            lightbox.removeChild(lightbox.firstChild);
+        }
+        //displays clicked image
+        lightbox.appendChild(img);
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return;
+    lightbox.classList.remove('active');
+})
